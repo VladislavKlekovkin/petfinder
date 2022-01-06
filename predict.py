78d604f50470dcd0b82c6f45121fa92c616f41ee
Vladislav <klekovkin.vladislav@gmail.com>
2022-01-07 02:39:32 +0700

    Added new models for testing

--- predict.py
+++ predict.py
@@ -47,9 +47,9 @@ oof_predictions = []
 targets = np.concatenate([df[df['fold'] == fold]['Pawpularity'].values for fold in range(Training.n_folds)]) / 100.
 
 testing_models = [('swin_large_patch4_window12_384_in22k_02-01-2022-23:53:35', 384),  # 1
-                  # ('swin_large_patch4_window12_384_02-01-2022-10:46:15', 384),        # 2
-                  # ('swin_large_patch4_window7_224_in22k_02-01-2022-04:51:06', 224),   # 3
-                  # ('swin_large_patch4_window7_224_29-12-2021-21:45:26', 224)          # 4
+                  ('swin_large_patch4_window12_384_02-01-2022-10:46:15', 384),        # 2
+                  ('swin_large_patch4_window7_224_in22k_02-01-2022-04:51:06', 224),   # 3
+                  ('swin_large_patch4_window7_224_29-12-2021-21:45:26', 224)          # 4
                  ]
 
 for model_name, image_size in testing_models:
