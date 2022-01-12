bae20ed27d6f19cd2f64bd57b5ce70a3db635072
Vladislav <vladislav.klekovkin@vicuesoft.com>
2022-01-12 19:33:45 +0700

    Bugfix

--- train-fastai.py
+++ train-fastai.py
@@ -84,7 +84,7 @@ def run(kernel_type, img_size):
     for i in range(N_FOLDS):
         print(f'Fold {i} results')
 
-        learn = get_learner(fold_num=i, kernel_type=kernel_type, img_size)
+        learn = get_learner(fold_num=i, kernel_type=kernel_type, img_size=img_size)
 
         learn.fit_one_cycle(5, 2e-5, cbs=[SaveModelCallback(fname=f'../{kernel_type}_fold_{i}'),
                                           EarlyStoppingCallback(monitor='petfinder_rmse', comp=np.less, patience=2)])
